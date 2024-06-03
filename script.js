9659cd564c76b9a929d79ff7b6b59abf2bec5128
Praval Pratap Singh <113175059+Pravalpratap@users.noreply.github.com>
2024-06-03 05:43:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+let main=document.querySelector(".container")
+let imgClassName=["img1","img2","img3","img4","img5"]
+let randomimg= imgClassName[parseInt(Math.random()*imgClassName.length]
+imgClassName.push(randomimg);
+
+for(let t of imgClassName){
+	let imgtag=document.createElement("img");
+	imgtag.className=t;
+	main.append(imgtag);
+	
+}

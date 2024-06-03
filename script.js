181b3982aab0c911276c4d9e8ec424c4148ab9d5
Praval Pratap Singh <113175059+Pravalpratap@users.noreply.github.com>
2024-06-03 06:08:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-//your code here
-let main=document.querySelector(".container")
-let imgClassName=["img1","img2","img3","img4","img5"]
-let randomimg= imgClassName[parseInt(Math.random()*imgClassName.length]
-imgClassName.push(randomimg);
+let mainContainer = document.querySelector(".container");
+let imageClassNames = ["img1", "img2", "img3", "img4", "img5"];
+let randomImageClass = imageClassNames[Math.floor(Math.random() * imageClassNames.length)];
+imageClassNames.push(randomImageClass);
 
-for(let t of imgClassName){
-	let imgtag=document.createElement("img");
-	imgtag.className=t;
-	main.append(imgtag);
-	
+let shuffledArray = [];
+let tempArray = [...imageClassNames]; // Create a temporary copy of imageClassNames
+
+while (shuffledArray.length < 6) {
+    let randomIndex = Math.floor(Math.random() * tempArray.length);
+    if (tempArray[randomIndex] === null) {
+        continue;
+    }
+    
+    shuffledArray.push(tempArray[randomIndex]);
+    tempArray[randomIndex] = null; // Mark this index as used by setting it to null
+}
+
+for (let className of shuffledArray) {
+    let imgElement = document.createElement("img");
+    imgElement.className = className;
+    mainContainer.append(imgElement);
 }
